b0a568256b66a4c667434300cadbe17385a4b15b
BenchPress200 <jms0538@naver.com>
2024-12-05 14:27:19 +0900

    chore: add react code

--- react-ts/src/pages/Home.tsx
+++ react-ts/src/pages/Home.tsx
@@ -1,23 +1,37 @@
-import styled from 'styled-components';
 
-const Container = styled.div`
-  width: 100%; 
-  background-color: #f0f0f0; 
-  margin: 0 auto; 
-`;
+import { useState, memo, useEffect, useRef, useMemo } from 'react';
 
-const Text = styled.div`
-  width: 50%; 
-  background-color: #ddd; 
-  text-align: center; 
-`;
 
 function Home() {
-    return (
-        <Container>
-            <Text>Hello world!</Text>
-        </Container>
-    );
+  const [counter, setCounter] = useState<number>(0);
+
+  function handleClick() {
+    setCounter((prev) => prev + 1);
+  }
+
+  useEffect(() => {
+    function addMouseEvent() {
+      console.log(counter);
+    }
+
+    window.addEventListener('click', addMouseEvent);
+
+    // 언마운트하고 리렌더링
+    return () => {
+      console.log('클린업 함수 실행!', counter)
+      window.removeEventListener('click', addMouseEvent);
+    }
+  }, [counter])
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button onClick={handleClick}>+</button>
+    </>
+  );
 }
+
+
+
 
 export default Home;
